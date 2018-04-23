1f3051dd527925c39a5a2589b14faf2503c96e8c
Huy Nguyen <weehuy@gmail.com>
2018-04-22 18:06:45 -0700

    Change to

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -105,7 +105,11 @@ class FG_eval {
       AD<double> delta0 = vars[delta_start + t - 1];
       AD<double> a0 = vars[a_start + t - 1];
 
-      AD<double> f0 = coeffs[0] + coeffs[1] * x0;
+      AD<double> f0 = coeffs[0];
+      for(int i = 1; i < coeffs.size(); i++) {
+        f0 += coeffs[i] * CppAD::pow(x0,i);
+      }
+
       AD<double> psides0 = CppAD::atan(coeffs[1]);
 
       // Here's `x` to get you started.
